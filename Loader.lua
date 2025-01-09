1fbcde80f223999c4aebe0e9b56518515edca50e
tempaccount42069 <mateohernandez2@proton.me>
2025-01-08 23:25:25 -0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,18 +1,31 @@
--- Define the specific Game ID to check
-local targetGameID = 118109254120801 -- Replace this with the desired game ID
+-- Define a table of target Game IDs and their respective actions
+local targetGameIDs = {
+    [118109254120801] = function()
+        -- Code for Game 1
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/tempaccount42069/bfbvgkaitun/refs/heads/main/BloxFruitsButVeryGood.lua"))()
+    end,
+    [987654321] = function()
+        -- Code for Game 2
+        print("Hello World from Game 2")
+    end,
+    [112233445] = function()
+        -- Code for Game 3
+        print("Hello World from Game 3")
+    end
+}
 
 -- Get the current game ID
 local currentGameID = game.PlaceId
 
--- Check if the current game ID matches the target ID
-if currentGameID == targetGameID then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/tempaccount42069/bfbvgkaitun/refs/heads/main/BloxFruitsButVeryGood.lua"))()
+-- Check if the current game ID matches any of the target Game IDs
+if targetGameIDs[currentGameID] then
+    -- Run the corresponding function for the matched game ID
+    targetGameIDs[currentGameID]()
 else
--- Send a notification if the game ID doesn't match
-game:GetService("StarterGui"):SetCore("SendNotification", {
-Title = "KitKat Hub";
-Text = "Game not supported D:";
-Duration = 5;
-})
+    -- Send a notification if the game ID doesn't match any of the target IDs
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "KitKat Hub";
+        Text = "Game not supported";
+        Duration = 5;
+    })
 end
-
